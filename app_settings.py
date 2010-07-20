eeca2f989bdbe4c5a0d902f94c9241dcec564f42
Jonathan Elsas <jelsas@cs.cmu.edu>
2010-07-19 23:12:45 -0400

    forgot to add app_settings.py

---
+++ app_settings.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+
+
+# A string format pattern with a placeholder (%s) for the document identifier
+EXTERNAL_URL_PATTERN = getattr(settings, 'EXTERNAL_URL_PATTERN',
+  'http://manchester.lti.cs.cmu.edu/testing/showdoc.php?documentID=%s')
+
+# Should the left & right documents be swapped randomly when presented?
+RANDOMIZE_DOC_PRESENTATION = getattr(settings, 'RANDOMIZE_DOC_PRESENTATION',
+                                     True)
+
+# Is a new registration required to provide a validation key?
+VALIDATION_KEY = getattr(settings, 'VALIDATION_KEY', None)
+
+
