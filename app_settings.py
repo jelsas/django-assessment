329efd1edca25bac82ee78a554ef39a2f170adc6
Jonathan Elsas <jelsas@cs.cmu.edu>
2010-07-21 14:30:23 -0400

    Change example URL pattern to a real example, not my actual server

--- app_settings.py
+++ app_settings.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 # A string format pattern with a placeholder (%s) for the document identifier
 EXTERNAL_URL_PATTERN = getattr(settings, 'EXTERNAL_URL_PATTERN',
-  'http://manchester.lti.cs.cmu.edu/testing/showdoc.php?documentID=%s')
+  'http://example.com/document_server.php?docid=%s')
 
 # Should the left & right documents be swapped randomly when presented?
 RANDOMIZE_DOC_PRESENTATION = getattr(settings, 'RANDOMIZE_DOC_PRESENTATION',
